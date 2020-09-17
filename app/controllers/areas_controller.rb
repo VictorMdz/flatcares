ff8d9027269a83c105e42188ef554ccb76f22057
Victor Mendoza <victormendoza1305@gmail.com>
2020-09-17 12:22:13 +0200

    Some changes in areas controller for tasks

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,5 +1,19 @@
 class AreasController < ApplicationController
+  before_action :set_area, only: [:show, :update, :edit, :destroy]
   def index
     @areas = Area.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @area = Area.find(params[:area_id])
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new
   end
 end

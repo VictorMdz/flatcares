2eb9b277779edf0122d8e0e17b2112733d00e58f
Mara Chimeno <marachimeno@gmail.com>
2020-09-17 15:02:46 +0200

    syntaxis changes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,36 @@
 class TasksController < ApplicationController
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to area_path(@task.area_id)
+    else
+      render area_path(@task.area_id)
+    end
+  end
+
+  def new
+    @area = Area.find(params[:area_id])
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    @area = Area.find(params[:area_id])
+    @task.area_id = @area.id
+
+    if @task.save
+      redirect_to area_path(@area)
+    else
+      render "tasks/new"
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:completed, :name)
+  end
 end

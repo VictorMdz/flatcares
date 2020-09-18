3c99e2cebe1906d447050e74d909ad7f598c9f3f
Camille Supery <csupery@gmail.com>
2020-09-18 15:14:26 +0200

    test

--- config/initializers/icons.rb
+++ config/initializers/icons.rb
@@ -6,3 +6,14 @@ BILL_ICONS = {
   gas: 'fas fa-burn',
   other: 'fas fa-dollar-sign'
 }
+
+AREAS_ICONS = {
+  kitchen: 'icon fas fa-utensils',
+  toilet: 'icon fas fa-toilet',
+  living_room: 'icon fas fa-couch',
+  corridor: 'icon fas fa-road',
+  stairs: 'icon fas fa-shoe-prints',
+  garden: 'icon fab fa-pagelines',
+  bathroom: 'fas fa-bath',
+  other: 'icon fas fa-home'
+}

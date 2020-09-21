b46e1108d6b9c42b94346fd69c9246eb0f8dee44
jantayreyes <jantayreyes@gmail.com>
2020-09-21 17:46:57 +0200

    Click on navbar chat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,6 +4,7 @@ class FlatsController < ApplicationController
   def show
     @flat_flatmembers =  @flat.flatmembers
     @flat_areas = @flat.areas
+    @chatroom = @flat.chatrooms.last
     @notifications = current_user.notifications
     @notifications = @notifications.sort_by(&:updated_at)
     @notifications.reverse!

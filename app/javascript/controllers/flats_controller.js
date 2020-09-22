02a37847d5b906b3372d5614a6c02739c7803cf5
Mara Chimeno <marachimeno@gmail.com>
2020-09-22 22:06:47 +0200

    edit name

--- app/javascript/controllers/flats_controller.js
+++ app/javascript/controllers/flats_controller.js
@@ -1,38 +1,38 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-
   static targets = ["title", "input"]
 
   changeNameToInput () {
+    console.log("Hello")
     if (this.titleTarget.querySelector('h1')) {
       this.titleTarget.innerHTML = `
-        <input autofocus type="text" data-action="blur->tasks#saveValueOfInput" class="input-group-text mb-3" data-target="tasks.input">
+        <input autofocus type="text" data-action="blur->flats#saveValueOfInput" class="input-group-text mb-3" data-target="flats.input">
       `
     }
   }
 
   saveValueOfInput () {
     console.log(this.titleTarget.dataset.id)
-    fetch(`/areas/${this.titleTarget.dataset.id}`, {
+    fetch(`/flats/${this.titleTarget.dataset.id}`, {
       headers: { accept: "application/json", 'content-type': 'application/json' },
       method: "PATCH",
       body: JSON.stringify({
-        area: {
+        flat: {
           name: this.inputTarget.value }
         }
       )
     })
       .then(response => response.json())
       .then((data) => {
-        this.changeInputToName(data.area.name);
+        this.changeInputToName(data.flat.name);
       });
   }
 
   changeInputToName (name) {
     if (this.titleTarget.querySelector('input')) {
       this.titleTarget.innerHTML = `
-        <h1 class="mb-3">${name} tasks</h1>
+        <h1 class="text-center">${name} tasks</h1>
       `
     }
   }

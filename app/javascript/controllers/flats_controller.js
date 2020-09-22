4682e0988104ef78c77b6099b9b392ff99399143
Mara Chimeno <marachimeno@gmail.com>
2020-09-22 22:27:05 +0200

    sintax error fix

---
+++ app/javascript/controllers/flats_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["title", "input"]
+
+  changeNameToInput () {
+    if (this.titleTarget.querySelector('h1')) {
+      this.titleTarget.innerHTML = `
+      <div class="mb-2" style="text-align:center;">
+        <input autofocus placeholder="edit flat name" type="text" data-action="blur->flats#saveValueOfInput" class="text-center" data-target="flats.input">
+      </div>
+      `
+    }
+  }
+
+  saveValueOfInput () {
+    fetch(`/flats/${this.titleTarget.dataset.id}`, {
+      headers: { accept: "application/json", 'content-type': 'application/json' },
+      method: "PATCH",
+      body: JSON.stringify({
+        flat: {
+          name: this.inputTarget.value }
+        }
+      )
+    })
+      .then(response => response.json())
+      .then((data) => {
+        this.changeInputToName(data.flat.name);
+      });
+  }
+
+  changeInputToName (name) {
+    if (this.titleTarget.querySelector('input')) {
+      this.titleTarget.innerHTML = `
+        <h1 class="text-center">${name}</h1>
+      `
+    }
+  }
+}

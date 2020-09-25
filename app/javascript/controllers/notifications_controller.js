e85ff0d49cf53eb36c4fe4432e7623aac7a171da
Mara Chimeno <marachimeno@gmail.com>
2020-09-25 13:48:16 +0200

    fixed popups

--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -2,31 +2,41 @@ import { Controller } from 'stimulus'
 import consumer from "../channels/consumer";
 import toastr from "toastr";
 
+const notified = new Set()
+
 export default class extends Controller {
 
-  static targets = ['container', 'card']
+  // static targets = ['container', 'card']
 
   connect() {
     this.initActionCable()
   }
 
   initActionCable() {
-    const container = this.containerTarget
-    const id = container.dataset.flatId
+    const container = document.querySelector('#notifications-wrapper')
+    const flatId = document.body.dataset.flat
+    const user = document.body.dataset.user
 
-    consumer.subscriptions.create({ channel: "NotificationChannel", id }, {
-      received(data) {
-        const userId = container.dataset.userId
-        container.insertAdjacentHTML('afterbegin', data.html)
-        if (userId != data.id.id) {
-          toastr.options.timeOut = 5000
-          toastr.options.onclick = () => window.location.href = data.url
-          // toastr.info(data.message)
-          toastr.success(data.message);
-          console.log(data.id.id)
-          console.log(userId)
-        }
-      },
-    });
+    if (flatId) {
+      consumer.subscriptions.create({ channel: "NotificationChannel", id: flatId }, {
+        received(data) {
+
+          if (container) {
+            container.insertAdjacentHTML('afterbegin', data.html)
+          }
+
+          if (notified.has(data.notification_id)) {
+            return
+          }
+
+          if (data.user.id != user) {
+            toastr.options.timeOut = 5000
+            toastr.options.onclick = () => window.location.href = data.url
+            toastr.success(data.message);
+            notified.add(data.notification_id)
+          }
+        },
+      });
+    }
   }
 }
